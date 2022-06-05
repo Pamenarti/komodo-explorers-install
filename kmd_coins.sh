b8928b077b370ea290ed306fe248198575d4efba
Paro <pamenarti@gmail.com>
2022-06-06 02:40:47 +0300

    s
    s

--- kmd_coins.sh
+++ kmd_coins.sh
@@ -4,7 +4,7 @@
 # mean REVS will have ports (8233, 8333, 3002), SUPERNET will have (8234, 8334, 3003),
 # etc. - you shouldn't remove dead coins from this list, to don't broke current setup,
 # especially if you have nginx configs assigned to that ports.
-declare -a kmd_coins=(LOTUS)
+declare -a kmd_coins=()
 # instead of removal dead coin from kmd_coins better to add them into disabled_coins
 # array
 declare -a disabled_coins=()
